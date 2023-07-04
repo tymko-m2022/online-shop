b10b092bc0d63439f596229331fcd91981767908
Yurk <Starovman@ukr.net>
2023-07-04 20:00:09 +0300

    add button theme toggle

--- front-end/src/app/components/shared/navbar/navbar.component.ts
+++ front-end/src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +10,21 @@ import { CartService } from 'src/app/services/cart.service';
 export class NavbarComponent implements OnInit {
   cartCount: number = 0;
 
-  constructor(private cartService: CartService){}
+  constructor(private cartService: CartService, private themeService: ThemeService){}
 
   ngOnInit(): void {
     this.cartService.cartCount$.subscribe((count) => {
       this.cartCount = count;
     });
   }
+
+  changeTheme () {
+    this.themeService.changeAndUpdateTheme();
+  }
+
+  getTheme () {
+    const newTheme: boolean = this.themeService.returnTheme();
+    return newTheme;
+  }
+
 }
