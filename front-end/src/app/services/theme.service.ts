1780a3ffb72e537bdec2c6f7750b89bb0f0c07d0
Yurk <Starovman@ukr.net>
2023-07-04 18:40:39 +0300

    add themeService

---
+++ front-end/src/app/services/theme.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+
+  private colorTheme: boolean;
+
+  constructor() { 
+    this.colorTheme = (() => {
+      if (localStorage.getItem('theme')) {
+
+        const memoryObject: object = JSON.parse(localStorage.getItem('theme')!);
+        if ("color" in memoryObject && typeof memoryObject.color === "boolean") {
+  
+          return memoryObject.color;
+  
+        }
+  
+      };
+  
+      return false;
+    })();
+  }
+
+  returnTheme () {
+    return this.colorTheme
+  }
+
+  updateTheme () {
+    localStorage.removeItem('theme');
+    const memoryObject = {
+      color: this.returnTheme()
+    };
+    localStorage.setItem('theme', JSON.stringify(memoryObject));
+  }
+
+}
